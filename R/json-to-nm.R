a97b16a468379c58444c28949550014704075eca
Sasha Crofter <sasha@crofter.org>
2013-01-22 19:08:32 -0500

    Add script for expanding and graphing JSON

---
+++ R/json-to-nm.R
@@ -0,0 +1,30 @@
+#!/usr/bin/Rscript
+# Check command line arguments
+if(length(jfile) == 0) {
+  jfile <- commandArgs(TRUE)[1]
+}
+if(length(jfile) == 0) {
+    # Exit if the file is not supplied
+    print("You must supply a file argument.")
+    quit(1)
+}
+
+# Load required libraries
+require("rjson")
+require("igraph")
+
+nm.json <- fromJSON(file=jfile)
+nm.graph <- c()
+
+# Expand the $network row into igraph form
+for(i in 1:length(nm.json$network)) {
+    if(length(nm.json$network[[i]] > 0)) {
+      for(j in 1:length(nm.json$network[[i]])) {
+          nm.graph <- c(nm.graph, i, nm.json$network[[i]][j])
+      }
+    }
+}
+
+# Create the network map
+g <- graph(nm.graph)
+plot(g)
