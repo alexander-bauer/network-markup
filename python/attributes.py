55c399db50a6bd444d10da696e07792b60f237f7
Sasha Crofter <sasha@crofter.org>
2013-01-26 22:19:35 -0500

    Add literal attributes in mapper
    This also simplifies the special attribute dictionary. Every "special"
    attribute is placed in a dictionary, mapped to another dictionary
    containing all of its literal vertex attribute mappings. For example:
    
        {'disabled': {'color': 'grey'}}
    
    This will map the attribute 'disabled' to vertex['color'] = 'grey'.

--- python/attributes.py
+++ python/attributes.py
@@ -1,5 +1,7 @@
 import igraph
 
+attb = {'disabled': {'color': 'grey'}}
+
 # Applies known attributes, such as "disabled" to vertices by changing
 # them into igraph-recognized values. The "label" attribute is
 # automatically set to the supplied name. It returns the modified
@@ -11,9 +13,17 @@ def apply(vertex, name, node):
     except Exception:
         return vertex
 
-    for name, key in attributes.items():
-        # If the node is disabled, then grey it out.
-        if name == 'disabled':
-            if key == True:
-                vertex["color"] = "grey"
+    for aName, aVal in attributes.items():
+        # If the attb dictionary contains the relevant attribute, then
+        # apply all changes implied.
+        try:
+            changes = attb[aName]
+            for cName, cVal in changes.items():
+                vertex[cName] = cVal
+        except Exception:
+            print "Adding attribute: using literal " + str(aName)
+            # If the change isn't known, though, try to apply it
+            # anyway. This is for cases in which the attributes are
+            # literal, like {'color':'blue'}
+            vertex[str(aName)] = aVal
     return vertex
