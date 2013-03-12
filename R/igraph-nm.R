9eb5286a20423045efc08ee2517485b74fc455bb
WilCrofter <bill@crofter.net>
2013-03-12 13:27:54 -0400

    Fix up igraph-nm.R
    Signed-off-by: Sasha Crofter <sasha@crofter.org>

--- R/igraph-nm.R
+++ R/igraph-nm.R
@@ -12,58 +12,19 @@ args <- getopt(arguments)
 background <- "white"
 
 j <- fromJSON(file=args[["file"]])
-g <- graph.empty(directed=args[["directedness"]])
+g <- graph.empty(n=length(j$nodes), directed=args[["directedness"]])
+# Add their names
+my.names <- V(g)$label <- V(g)$name <- names(j$nodes)
 
-# Adding vertices
-print("Creating vertices")
-names <- names(j$nodes)
-for(n in 1:length(j$nodes)) {
-  print(paste("Adding vertex:", names[n]))
-  add.vertices(g, 1, attr=list(label=names[n]))
-}
+g <- add.edges(g, c("Oberon", "Theseus"), attr=list(label="test"))
 
 # Now add edges
-print("Creating edges")
-for(n in 1:length(j$nodes)) {
-  print(paste("For node", n))
-  print(j$nodes[n])
-  print(j$nodes[n]$connected)
-  for(connection in j$nodes[n]$connected) {
-	print(paste("Adding edge:", names[n], "--", connection$target))
-  	add.edge(g, edge(names[n], connection$target),
-	  attr=list(label=connection$attributes$label))
+for(n in 1:length(j$nodes)){
+  node <- j$nodes[n][[1]]
+  for(connection in node$connected){
+    g <- add.edges(g, c(my.names[n], connection$target),
+                      attr=connection$attributes)
   }
 }
 
-quit(0, save="no")
-
-## Old Code
-
-graphNJSON <- function(jfile, directed=TRUE) {
-  # Requires "rjson" and "igraph"
-  
-  nm.json <- fromJSON(file=jfile)
-  nm.graph <- c()
-  
-  # Initialize the graph with the given nodes
-  g <- graph.empty(n=length(nm.json), directed=directed)
-  # Add their names
-  V(g)$name <- names(nm.json)
-  V(g)$label <- V(g)$name
-  
-  # Now, add the edges
-  for(i in 1:length(nm.json)) {
-    # If the node has a "connected" field,
-    # then we note the connections by looking
-    # the names up.
-    if(length(nm.json[[i]]$connected > 0)) {
-      for(j in 1:length(nm.json[[i]]$connected)) {
-        # Add the entry
-        g <- g + edge(names(nm.json)[i],
-                        nm.json[[i]]$connected[j])
-      }
-    }
-  }
-  
-  plot(g, vertex.label.dist=1.5)
-}
+plot(g)
